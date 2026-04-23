08f72ddf772a0ac462ab12cc1da2e5966e08f878
ali-nasser03 <abujaber4803@gmail.com>
2026-04-23 20:44:45 +0300

    Update FootballManagerApplication.java

--- src/main/java/com/team/football_manager/FootballManagerApplication.java
+++ src/main/java/com/team/football_manager/FootballManagerApplication.java
@@ -2,12 +2,13 @@ package com.team.football_manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class FootballManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FootballManagerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(FootballManagerApplication.class, args);
+    }
 }
