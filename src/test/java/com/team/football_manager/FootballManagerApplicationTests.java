50af49e3ae69a929746591a76ca74e06ce8c69de
ali-nasser03 <abujaber4803@gmail.com>
2026-04-23 20:35:30 +0300

    Update FootballManagerApplicationTests.java

--- src/test/java/com/team/football_manager/FootballManagerApplicationTests.java
+++ src/test/java/com/team/football_manager/FootballManagerApplicationTests.java
@@ -1,7 +1,10 @@
 package com.team.football_manager;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
 
 @SpringBootTest
 class FootballManagerApplicationTests {
