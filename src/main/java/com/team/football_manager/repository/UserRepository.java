667c9ea6e07f6cee2b7d6e04c2d2cef3ac93b3a7
ali-nasser03 <abujaber4803@gmail.com>
2026-04-27 12:37:43 +0300

    Update UserRepository.java

--- src/main/java/com/team/football_manager/repository/UserRepository.java
+++ src/main/java/com/team/football_manager/repository/UserRepository.java
@@ -2,13 +2,9 @@ package com.team.football_manager.repository;
 
 import com.team.football_manager.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    Optional<User> findByFullName(String fullName);
-
-    List<User> findByRole(String role);
+    Optional<User> findByFullNameIgnoreCase(String fullName);
+    Optional<User> findByUsernameIgnoreCase(String username);
 }
