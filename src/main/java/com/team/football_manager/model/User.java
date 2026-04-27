d66c5f715e656b8c6beb864dcac0b063fbe70c1b
ali-nasser03 <abujaber4803@gmail.com>
2026-04-27 12:21:14 +0300

    Update User.java

--- src/main/java/com/team/football_manager/model/User.java
+++ src/main/java/com/team/football_manager/model/User.java
@@ -1,75 +1,24 @@
 package com.team.football_manager.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
 @Table(name = "app_users")
+@Data
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String username;
-    private int age;
-    private String password;
-
     @Column(name = "full_name")
     private String fullName;
 
-    private String role;
+    private String username;
+    private int age;
+    private String password;
+    private String role; // ADMIN or PLAYER
 
-    private boolean working;
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public boolean isWorking() {
-        return working;
-    }
-
-    public void setWorking(boolean working) {
-        this.working = working;
-    }
+    // الحقل الجديد
+    private boolean exempt = false; 
 }
